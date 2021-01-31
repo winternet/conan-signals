1e573476aef0d2088ff1e041158d92d79cc75f1f
Christoph Winter <mail.christoph.winter@gmail.com>
2021-01-31 19:27:18 +0100

    exchange remote

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -4,7 +4,7 @@
 set -e
 set -x
 
-conan remote add conan-priv "https://api.bintray.com/conan/winternet/${CONAN_REMOTE}"
+conan remote add ${CONAN_REMOTE} "https://api.bintray.com/conan/winternet/${CONAN_REMOTE}"
 conan user -p ${CONAN_API_KEY} -r ${CONAN_REMOTE} ${CONAN_USER}
 conan create . signals/0.0.1@winternet/testing
 conan upload signals/0.0.1@winternet/testing -r ${CONAN_REMOTE}
